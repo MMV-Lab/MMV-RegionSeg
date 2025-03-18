03bd8a0df5db52b990898a87b3af3a104dc85af2
Peter Lampen <lampen@isas.de>
2025-03-18 16:45:47 +0100

    flood_fill replaced ba flood

--- src/mmv_regionseg/_widget.py
+++ src/mmv_regionseg/_widget.py
@@ -32,10 +32,10 @@ class mmv_regionseg(QWidget):
         self.viewer = viewer
         self.stem = ''
         self.image = None
-        self.new_image = None
+        self.mask = None
         self.tolerance = 10
         self.class1 = 1
-        self.pos = None
+        self.seed_points = []
 
         # Define a vbox for the main widget
         vbox = QVBoxLayout()
@@ -57,6 +57,7 @@ class mmv_regionseg(QWidget):
         sld_tolerance.valueChanged.connect(self.tolerance_changed)
         vbox.addWidget(sld_tolerance)
 
+        """
         # Slider for the class
         self.lbl_class = QLabel('Class: 1')
         vbox.addWidget(self.lbl_class)
@@ -64,14 +65,15 @@ class mmv_regionseg(QWidget):
         sld_class.setRange(1, 50)
         sld_class.valueChanged.connect(self.class_changed)
         vbox.addWidget(sld_class)
+        """
 
-        # Button select start point
-        btn_start = QPushButton('Select start point')
-        btn_start.clicked.connect(self.start_point)
-        vbox.addWidget(btn_start)
+        # Button select seed point
+        btn_seed_point = QPushButton('Select seed points')
+        btn_seed_point.clicked.connect(self.new_seed_point)
+        vbox.addWidget(btn_seed_point)
 
         # Button start floot_fill
-        btn_floot = QPushButton('Floot fill')
+        btn_floot = QPushButton('Floot')
         btn_floot.clicked.connect(self.start_floot)
         vbox.addWidget(btn_floot)
 
@@ -107,12 +109,14 @@ class mmv_regionseg(QWidget):
         self.tolerance = value
         self.lbl_tolerance.setText('Tolerance: %d' % (value))
 
+    """
     def class_changed(self, value: int):
         # (07.03.2025)
         self.class1 = value
         self.lbl_class.setText('Class: %d' % (value))
+    """
 
-    def start_point(self):
+    def new_seed_point(self):
         if self.stem != '':
             layer = self.viewer.layers[self.stem]
             layer.mouse_drag_callbacks.append(self.on_click)
@@ -122,18 +126,21 @@ class mmv_regionseg(QWidget):
     def on_click(self, layer, event):
         # (07.03.2025)
         # Retrieve mouse-click coordinates
-        pos = event.position
+        point = event.position
 
         # Convert the float tuple into an integer tuple
-        self.pos = tuple(map(int, pos))
+        point = tuple(map(int, point))
+        value = self.image[point]
+        self.seed_points.append(point)
 
-        print('Clicked at:', self.pos)
+        print('Clicked at:', point, 'value:', value)
 
     def start_floot(self):
         # (07.03.2025)
-        # Call flood_fill()
-        self.new_image = flood_fill(self.image, self.pos, self.class1,
-            tolerance=self.tolerance)
+        self.mask = np.zeros(self.image.shape, dtype=int)
+        for point in self.seed_points:
+            mask1 = flood(self.image, point, tolerance=self.tolerance)
+            self.mask += mask1
 
-        name1 = 'new_' + self.stem
-        self.viewer.add_image(self.new_image, name=name1)
+        name1 = 'mask'
+        self.viewer.add_labels(self.mask, name=name1)
