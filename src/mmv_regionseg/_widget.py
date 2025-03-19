50dc0d41acb290f8ce354d98b6e2e5adc740c4c6
Peter Lampen <lampen@isas.de>
2025-03-19 16:34:48 +0100

    deactivate callback function on_click

--- src/mmv_regionseg/_widget.py
+++ src/mmv_regionseg/_widget.py
@@ -30,49 +30,47 @@ class mmv_regionseg(QWidget):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__()
         self.viewer = viewer
-        self.stem = ''
+        self.name = None
         self.image = None
         self.mask = None
         self.tolerance = 10
-        self.class1 = 1
+        self.color = 0
         self.seed_points = []
 
         # Define a vbox for the main widget
         vbox = QVBoxLayout()
         self.setLayout(vbox)
-        self.lbl_headline = QLabel('MMV Region Segmentation')
-        vbox.addWidget(self.lbl_headline)
 
-        # Button read image
+        # Headline 'MMV REgion Segmentation'
+        lbl_headline = QLabel('MMV Region Segmentation')
+        vbox.addWidget(lbl_headline)
+
+        # Button 'Read image'
         btn_read = QPushButton('Read image')
         btn_read.clicked.connect(self.read_image)
         vbox.addWidget(btn_read)
 
-        # Slider for the tolerance
+        # Label 'Tolerance: x'
         self.lbl_tolerance = QLabel('Tolerance: 10')
         vbox.addWidget(self.lbl_tolerance)
+
+        # Slider for the tolerance
         sld_tolerance = QSlider(Qt.Horizontal)
         sld_tolerance.setRange(1, 50)
         sld_tolerance.setValue(10)
         sld_tolerance.valueChanged.connect(self.tolerance_changed)
         vbox.addWidget(sld_tolerance)
 
-        """
-        # Slider for the class
-        self.lbl_class = QLabel('Class: 1')
-        vbox.addWidget(self.lbl_class)
-        sld_class = QSlider(Qt.Horizontal)
-        sld_class.setRange(1, 50)
-        sld_class.valueChanged.connect(self.class_changed)
-        vbox.addWidget(sld_class)
-        """
+        # Button 'Select seed points'
+        btn_seed_points = QPushButton('Select seed points')
+        btn_seed_points.clicked.connect(self.new_seed_points)
+        vbox.addWidget(btn_seed_points)
 
-        # Button select seed point
-        btn_seed_point = QPushButton('Select seed points')
-        btn_seed_point.clicked.connect(self.new_seed_point)
-        vbox.addWidget(btn_seed_point)
+        # Note
+        lbl_note = QLabel('To select, use the right mouse button')
+        vbox.addWidget(lbl_note)
 
-        # Button start floot_fill
+        # Button 'Start floot'
         btn_floot = QPushButton('Floot')
         btn_floot.clicked.connect(self.start_floot)
         vbox.addWidget(btn_floot)
@@ -87,60 +85,73 @@ class mmv_regionseg(QWidget):
             return
         else:
             path = Path(filename)
-            self.stem = path.stem               # Name of the file
-            suffix = path.suffix.lower()        # File extension
+            self.name = path.stem               # Name of the file
+            extension = path.suffix.lower()     # File extension
 
         # Load the image file
-        print('Load', path)
-        try:
-            if suffix == '.tif' or suffix == '.tiff':
-                self.image = imread(path)
-            else:
-                print('Unknown file type: %s%s!' % (self.stem1, suffix))
-                return
-        except BaseException as error:
-            print('Error:', error)
+        if extension != '.tif' and extension != '.tiff':
+            print('Unknown file type: %s!' % (extension))
             return
+        else:
+            print('Load', path)
+            try:
+                self.image = imread(path)
+            except BaseException as error:
+                print('Error:', error)
+                return
 
-        self.viewer.add_image(self.image, name=self.stem)   # Show the image
+        self.viewer.add_image(self.image, name=self.name)   # Show the image
 
     def tolerance_changed(self, value: int):
         # (06.03.2025)
         self.tolerance = value
         self.lbl_tolerance.setText('Tolerance: %d' % (value))
 
-    """
-    def class_changed(self, value: int):
+    def new_seed_points(self):
         # (07.03.2025)
-        self.class1 = value
-        self.lbl_class.setText('Class: %d' % (value))
-    """
-
-    def new_seed_point(self):
-        if self.stem != '':
-            layer = self.viewer.layers[self.stem]
+        # Defines the callback function on_click
+        if self.name != None:
+            layer = self.viewer.layers[self.name]
             layer.mouse_drag_callbacks.append(self.on_click)
+
+            # select the image layer as active
+            self.viewer.layers.selection.active = layer
         else:
             print('Can\'t find an image!')
 
     def on_click(self, layer, event):
         # (07.03.2025)
         # Retrieve mouse-click coordinates
-        point = event.position
+        if event.type == 'mouse_press' and event.button == 2:
+            point = event.position
+        else:
+            return
 
         # Convert the float tuple into an integer tuple
-        point = tuple(map(int, point))
-        value = self.image[point]
+        iterator = map(int, point)
+        point = tuple(iterator)
         self.seed_points.append(point)
 
-        print('Clicked at:', point, 'value:', value)
+        color = self.image[point]
+        print('Clicked at:', point, 'color:', color)
 
     def start_floot(self):
         # (07.03.2025)
+        # Delete the callback function on_click
+        if self.name != None:
+            layer = self.viewer.layers[self.name]
+            layer.mouse_drag_callbacks.remove(self.on_click)
+
+        # Determine a mask that corresponds to a flood_fill
+        self.color += 1
         self.mask = np.zeros(self.image.shape, dtype=int)
         for point in self.seed_points:
             mask1 = flood(self.image, point, tolerance=self.tolerance)
-            self.mask += mask1
+            mask2 = mask1.astype(int) * self.color
+            self.mask += mask2
+
+        # Delete the seed points
+        self.seed_points = []
 
         name1 = 'mask'
         self.viewer.add_labels(self.mask, name=name1)
