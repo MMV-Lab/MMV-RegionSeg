38c9ebf67ac6d345a06fb2c16c98fc6e67f13c09
Peter Lampen <lampen@isas.de>
2025-03-20 17:40:32 +0100

    Modified config files and new test_widget.py

--- src/mmv_regionseg/_tests/test_widget.py
+++ src/mmv_regionseg/_tests/test_widget.py
@@ -1,66 +1,41 @@
+# Copyright © Peter Lampen, ISAS Dortmund, 2025
+# (20.03.2025)
+
+import pytest
+import napari
 import numpy as np
+import qtpy
+from qtpy.QtWidgets import QVBoxLayout
+from qtpy.QtTest import QTest
+from qtpy.QtCore import Qt
+from unittest import mock
+from unittest.mock import patch
+from pathlib import Path
+from tifffile import imread, imwrite
+from mmv_regionseg._widget import mmv_regionseg
 
-from mmv_regionseg._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
-)
-
-
-def test_threshold_autogenerate_widget():
-    # because our "widget" is a pure function, we can call it and
-    # test it independently of napari
-    im_data = np.random.random((100, 100))
-    thresholded = threshold_autogenerate_widget(im_data, 0.5)
-    assert thresholded.shape == im_data.shape
-    # etc.
-
+# A single constant
+PARENT = Path(__file__).parent / 'data'
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
-# you don't need to import it, as long as napari is installed
-# in your testing environment
-def test_threshold_magic_widget(make_napari_viewer):
+# you don't need to import it, as long as napari is installed in your
+# testing environment
+@pytest.fixture
+def region_seg(make_napari_viewer):
+    # (20.03.2025) create a mmv_regionseg object and give it back
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    return mmv_regionseg(viewer)
 
-    # our widget will be a MagicFactory or FunctionGui instance
-    my_widget = threshold_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    thresholded = my_widget(viewer.layers[0], 0.5)
-    assert thresholded.shape == layer.data.shape
-    # etc.
-
-
-def test_image_threshold_widget(make_napari_viewer):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-    my_widget = ImageThreshold(viewer)
-
-    # because we saved our widgets as attributes of the container
-    # we can set their values without having to "interact" with the viewer
-    my_widget._image_layer_combo.value = layer
-    my_widget._threshold_slider.value = 0.5
-
-    # this allows us to run our functions directly and ensure
-    # correct results
-    my_widget._threshold_im()
-    assert len(viewer.layers) == 2
-
-
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
+@pytest.mark.init
+def test_init(region_seg):
+    # (12.09.2024)
+    assert isinstance(region_seg, mmv_regionseg)
+    assert region_seg.name == None
+    assert region_seg.image == None
+    assert region_seg.mask == None
+    assert region_seg.tolerance == 10
+    assert region_seg.color == 0
+    assert region_seg.seed_points == []
+    assert region_seg.layout() is not None
+    assert isinstance(region_seg.layout(), QVBoxLayout)
+    assert region_seg.lbl_tolerance.text() == 'Tolerance: 10'
